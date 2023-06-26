70d7e04f5530a4732aca6020a712465dcd8cc9b1
Артем <artemvorkhlik@mail.ru>
2023-06-26 17:17:47 +0300

    setup project

--- RenWeather/MainViewController.swift
+++ RenWeather/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RenWeather
 //
 //  Created by Артём on 26.06.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
